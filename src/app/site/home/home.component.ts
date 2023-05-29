2e377eaf8871085a6c68a505430411a40d35eaf7
Daniel Swater <daniel.castro@softtrade.com.br>
2023-05-29 11:04:32 -0300

    ajustes na geolocalização

--- src/app/site/home/home.component.ts
+++ src/app/site/home/home.component.ts
@@ -5,9 +5,9 @@ import { UsuariosService } from './service/usuarios.service';
 import { NgxSpinnerService } from "ngx-spinner";
 import { CadastroUsuario } from './interface/CadastroUsuario.interface';
 import { Address } from 'ngx-google-places-autocomplete/objects/address';
-import firebase from 'firebase/compat/app';
-import { getFirestore, GeoPoint } from 'firebase/firestore';
 import 'firebase/firestore';
+import { GeoPoint } from 'firebase/firestore';
+declare var google: any;
 
 @Component({
   selector: 'app-home',
@@ -57,66 +57,61 @@ export class HomeComponent implements OnInit {
   }
 
   handleAddressChange(address: Address) {
-    const streetName = this.extractStreetName(address);
-    const neighborhood = this.extractNeighborhood(address);
-    const city = this.extractCity(address);
-    const state = this.extractState(address);
-  
-    const fullAddress = `${streetName}, ${neighborhood}, ${city}, ${state}`;
-  
-    const apiUrl = 'https://nominatim.openstreetmap.org/search?';
-    const params = {
-      q: fullAddress,
-      format: 'json',
-      limit: '1'
-    };
-  
-    this.httpClient.get<any[]>(apiUrl, { params }).subscribe((response) => {
-      console.log('response', response)
-      if (response.length > 0) {
-        const latitude = parseFloat(response[0].lat);
-        const longitude = parseFloat(response[0].lon);
-        console.log(latitude, longitude);
-  
+    // const streetName = this.extractStreetName(address);
+    // const neighborhood = this.extractNeighborhood(address);
+    // const city = this.extractCity(address);
+    // const state = this.extractState(address);
+
+    const fullAddress = address.formatted_address
+
+    console.log(fullAddress)
+
+    const geocoder = new google.maps.Geocoder();
+    geocoder.geocode({ address: fullAddress }, (results: any, status: any) => {
+      if (status === google.maps.GeocoderStatus.OK && results.length > 0) {
+        const location = results[0].geometry.location;
+        const latitude = location.lat();
+        const longitude = location.lng();
+
         const geopoint = new GeoPoint(latitude, longitude);
         this.modelCadastroUsuario.geolocalizacao = geopoint;
-        console.log('this.modelCadastroUsuario.geolocalizacao', this.modelCadastroUsuario.geolocalizacao)
+        console.log('this.modelCadastroUsuario.geolocalizacao', this.modelCadastroUsuario.geolocalizacao);
       }
     });
   }
 
 
-  extractStreetName(address: Address): string {
-    const routeComponent = address.address_components.find(component =>
-      component.types.includes('route')
-    );
+  // extractStreetName(address: Address): string {
+  //   const routeComponent = address.address_components.find(component =>
+  //     component.types.includes('route')
+  //   );
 
-    return routeComponent ? routeComponent.long_name : '';
-  }
+  //   return routeComponent ? routeComponent.long_name : '';
+  // }
 
-  extractNeighborhood(address: Address): string {
-    const neighborhoodComponent = address.address_components.find(component =>
-      component.types.includes('sublocality') || component.types.includes('neighborhood')
-    );
+  // extractNeighborhood(address: Address): string {
+  //   const neighborhoodComponent = address.address_components.find(component =>
+  //     component.types.includes('sublocality') || component.types.includes('neighborhood')
+  //   );
 
-    return neighborhoodComponent ? neighborhoodComponent.long_name : '';
-  }
+  //   return neighborhoodComponent ? neighborhoodComponent.long_name : '';
+  // }
 
-  extractCity(address: Address): string {
-    const cityComponent = address.address_components.find(component =>
-      component.types.includes('locality')
-    );
+  // extractCity(address: Address): string {
+  //   const cityComponent = address.address_components.find(component =>
+  //     component.types.includes('locality')
+  //   );
 
-    return cityComponent ? cityComponent.long_name : '';
-  }
+  //   return cityComponent ? cityComponent.long_name : '';
+  // }
 
-  extractState(address: Address): string {
-    const stateComponent = address.address_components.find(component =>
-      component.types.includes('administrative_area_level_1')
-    );
+  // extractState(address: Address): string {
+  //   const stateComponent = address.address_components.find(component =>
+  //     component.types.includes('administrative_area_level_1')
+  //   );
 
-    return stateComponent ? stateComponent.long_name : '';
-  }
+  //   return stateComponent ? stateComponent.long_name : '';
+  // }
 
   salvar() {
     // this.spinner.show()
